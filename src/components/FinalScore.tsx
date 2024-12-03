6a909962ecd1faab6f9a521775364403610b28d3
xz <xuezhen00@163.com>
2024-12-04 03:47:24 +0800

    更新title

---
+++ src/components/FinalScore.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const FinalScore: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="text-4xl font-bold text-gray-800">
+        得分 100
+      </div>
+    </div>
+  );
+}; 
