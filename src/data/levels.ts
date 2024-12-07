7a28b4c2ccdb3dfb7a56062419e5d6de706904b7
xz <xuezhen00@163.com>
2024-12-07 12:28:28 +0800

    Merge remote-tracking branch 'origin/main'

--- src/data/levels.ts
+++ src/data/levels.ts
@@ -6,7 +6,7 @@ export interface Level {
 export const levels: Level[] = [
   {
     targetAngle: 90,
-    instruction: "让我们从最常见的直角开始！试着旋转红线，使其形成90度角。"
+    instruction: "让我们从最常见的角"直角"开始！试着旋转红线，使其形成90度角。"
   },
   {
     targetAngle: 45,
